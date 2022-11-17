35f66612b0213d5dc7f8081b2a07902614454222
Viviane Dias <vivianedias@protonmail.com>
2022-11-17 16:35:09 -0300

    feat(legend): adds legend to map

---
+++ shared/components/Legend.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Divider,
+  HStack,
+  ListItem,
+  Text,
+  List,
+  VStack,
+} from "@chakra-ui/react";
+import { useTranslation } from "next-i18next";
+import { INFO_ACCESS, INFO_COLORS } from "../utils/buildCaseFilters";
+
+export default function Legend() {
+  const { t } = useTranslation("home");
+
+  return (
+    <Box
+      position="absolute"
+      width={"180px"}
+      height={"fit-content"}
+      bottom={"40px"}
+      left={"50px"}
+      backgroundColor={"brand.light"}
+      rounded={"md"}
+      boxShadow="md"
+      px={6}
+      py={3}
+    >
+      <VStack spacing={4}>
+        <Text
+          fontWeight={600}
+          fontSize={"sm"}
+          color={"brand.primary"}
+          lineHeight={4}
+        >
+          {t("legend.title")}
+        </Text>
+        <Divider borderColor={"brand.pink"} borderWidth={"0.5px"} />
+        <List spacing={3}>
+          {Object.keys(INFO_ACCESS).map((label, i) => {
+            return (
+              <ListItem key={`${label}-${i}`}>
+                <HStack spacing={2}>
+                  <Box
+                    minWidth={"20px"}
+                    height={"12px"}
+                    backgroundColor={INFO_COLORS[i]}
+                  />
+                  <Text
+                    fontSize={"xs"}
+                    color={"brand.primary"}
+                    fontWeight={500}
+                    lineHeight={4}
+                  >
+                    {t(`legend.${label.toLowerCase()}`)}
+                  </Text>
+                </HStack>
+              </ListItem>
+            );
+          })}
+        </List>
+      </VStack>
+    </Box>
+  );
+}
