8da32e7dff362ba2e7bb2a10c9b75b7028dec689
Camila Cardoso <caamila.cardoso.l@gmail.com>
2022-11-19 16:34:00 -0300

    feat: adds footer

--- shared/components/Footer.tsx
+++ shared/components/Footer.tsx
@@ -1,5 +1,8 @@
+import { Box, Flex, HStack } from "@chakra-ui/react";
 import { useTranslation } from 'next-i18next';
 import { IS_IN_MAINTENANCE } from '../utils/constants';
+
+import Logo from "./Header/Logo"
 
 export default function Footer() {
   const { t } = useTranslation('footer');
@@ -9,6 +12,25 @@ export default function Footer() {
   }
 
   return (
-    <footer>{t("title")}</footer>
+    <Box width={"100%"} as="header">
+      <Flex
+        bg={"black"}
+        minH={{ base: "190px", md: "385px" }}
+        w={"100%"}
+        align={"center"}
+        justify={"center"}
+      >
+        <HStack
+          maxW={{ base: "550px", md: "1080px" }}
+          w={"90%"}
+          align={{ base: "flex-start", md: "center" }}
+          justify={{ base: "space-between", md: "start"}}
+          spacing={20}
+          paddingRight={{ base: "15px", md: "0" }}
+        >
+          <Logo t={t} />
+        </HStack>
+      </Flex>
+    </Box>
   )
 }
