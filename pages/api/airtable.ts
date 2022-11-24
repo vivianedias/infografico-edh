ef674ccbb44371ae2bf5bc78b68124a6e05f10c9
Viviane Dias <vivianedias@pm.me>
2022-11-24 12:47:51 -0300

    feat(airtable): fetch states, timeline and orgaos tables from airtable

--- pages/api/airtable.ts
+++ pages/api/airtable.ts
@@ -1,8 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import fetcher from "../../shared/utils/fetcher";
-import { Records, Response } from "../../shared/types/airtable";
+import {
+  OrgaosResponse,
+  Records,
+  Response,
+  StatesResponse,
+  TimelineResponse,
+} from "../../shared/types/airtable";
 import { log } from "next-axiom";
-import { AIRTABLE_PROD_VIEW_ID, AIRTABLE_STG_VIEW_ID, env } from "../../shared/utils/constants";
+import {
+  AIRTABLE_PROD_VIEW_ID,
+  AIRTABLE_STG_VIEW_ID,
+  env,
+} from "../../shared/utils/constants";
 
 const parse = ({ records }: { records: Records[] }) => {
   return records.map((record: Records) => {
@@ -14,28 +24,62 @@ const parse = ({ records }: { records: Records[] }) => {
   });
 };
 
+function addOrgaosToStatesData(
+  states: StatesResponse[],
+  orgaos: OrgaosResponse[]
+) {
+  return states.map((s) => {
+    const orgaosByState = orgaos.filter(
+      (o) => o.orgao__estado === s.estado__sigla
+    );
+    return {
+      ...s,
+      orgaos: orgaosByState,
+    };
+  });
+}
+
+async function fetchTable(endpoint: string) {
+  const token = process.env.AIRTABLE_API_KEY;
+  const opts = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    isExternal: true,
+  };
+
+  const data = await fetcher(endpoint, opts);
+
+  return parse(data);
+}
+
+function getEndpoint(tableName: string, lng: string | string[]) {
+  const tableId =
+    env !== "production" ? AIRTABLE_STG_VIEW_ID : AIRTABLE_PROD_VIEW_ID;
+
+  return `https://api.airtable.com/v0/${tableId}/${tableName}__${lng}?maxRecords=100&view=Grid%20view`;
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Response[] | string>
+  res: NextApiResponse<Response | string>
 ) {
   try {
     const { query } = req;
-    const tableId =
-      env !== "production" ? AIRTABLE_STG_VIEW_ID : AIRTABLE_PROD_VIEW_ID
+    const lng = query.lng || "pt-BR";
 
-    const endpoint = `https://api.airtable.com/v0/${tableId}/estados__${query.lng}?maxRecords=100&view=Grid%20view`;
+    const stateEndpoint = getEndpoint("estados", lng);
+    const timelineEndpoint = getEndpoint("timeline", lng);
+    const orgaosEndpoint = getEndpoint("orgaos", lng);
 
-    const token = process.env.AIRTABLE_API_KEY;
-    const opts = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      isExternal: true,
-    };
+    const states = (await fetchTable(stateEndpoint)) as StatesResponse[];
+    const timeline = (await fetchTable(timelineEndpoint)) as TimelineResponse[];
+    const orgaos = (await fetchTable(orgaosEndpoint)) as OrgaosResponse[];
 
-    const data = await fetcher(endpoint, opts);
-
-    res.status(200).json(parse(data));
+    res.status(200).json({
+      timeline,
+      tableData: addOrgaosToStatesData(states, orgaos),
+    });
   } catch (e) {
     log.error(`Request to airtable API failed`, e);
 
